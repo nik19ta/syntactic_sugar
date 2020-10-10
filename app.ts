3109489d27afe5f7843faf5657eac9c4d7071f2c
nik19ta <nikhvatov19@gmail.com>
2020-10-10 03:13:35 +0300

    add new func

--- app.ts
+++ app.ts
@@ -1,28 +1,26 @@
-// random
-
 const randfloat = (min: number, max: number): number => {
     return Math.random() * (max - min) + min;
 };
 const randint = (min: number, max: number): number => {
     return +(Math.random() * (max - min) + min).toFixed(0)
-}
+};
 const randchoice = (arr: any): any => {
     return arr[Math.floor(Math.random() * arr.length)]
-}
+};
 const floor = (num: number): number => {
     return Math.floor(num);
-}
+};
 const str = (data: number): string => {
     return data.toString();
-}
+};
 const int = (num: string): number => {
     return +num;
-}
+};
 const float = (num: string): number => {
     return +(num);
 }
 const tf = (num: any, poin: number, type: boolean): number => {
-    return type ? num.toFixed(poin): +num.toFixed(poin);
+    return type ? num.toFixed(poin) : +num.toFixed(poin);
 }
 const sort = (arr: any): any => {
     return arr.sort()
@@ -33,33 +31,21 @@ const reverse = (arr: any) => {
 const type = (data: any): any => {
     return typeof data;
 }
-const inStr = (text: string, word: string) => {
+const inStr = (text: string, word: string): boolean => {
     return text.indexOf(word) !== -1;
 }
-const is = (condition:boolean, callback:any): void => { 
-    condition ? callback(): ''
+const is = (condition: boolean, callback: any): void => {
+    condition ? callback() : ''
 };
 
-
-// -- tests --
-// console.log(randint(12,45))
-// console.log(randint(12,45))
-// console.log(randchoice([23, 34, 87, 2, 3456, 76]));
-// console.log(randchoice(['weer','werf','werfg','jhgfd']));
-// console.log(floor(234.345));
-// console.log(str(34));
-// console.log(int('234'));
-// console.log(float('234.4'));
-// console.log(float(234));
-// console.log(tf(234.3445, 0, false));
-// console.log(sort([1,234,65,32]));
-// console.log(reverse([1, 234, 65, 32]));
-// console.log(type(false));
-// console.log(inStr('wedfg', 'w'));
-// is(true, () => {
-//     console.log('nik')
-// })
-
-
-
-
+const esc2Uni = (str: string): string => {
+    let escapedCodeTemp = str.replace(/\\u([\d\w]{4})/gi, (match, group) =>
+        String.fromCharCode(parseInt(group, 16)));
+    return unescape(escapedCodeTemp);
+}
+const delStr = (text: string, param1: string): string => {
+    return text.replace(param1, '')
+};
+const getText = (text: string, param1: string, param2: string): string => {
+    return text.split(param1)[1].split(param2)[0]
+};
